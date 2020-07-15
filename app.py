9cd6913742b2b994a9674dce60ac9c133bca1271
Robert Pagano <robert.pagano@thespurgroup.com>
2020-07-15 15:11:18 -0700

    adding back torch and torchvision to requirements and uncommenting stuff out

--- app.py
+++ app.py
@@ -36,58 +36,58 @@ def forbidden_handler(e: HTTPException):
 def request_timeout_handler(e: HTTPException):
     return render_template('408.html'), 408
     
-# ## THIS ONE WORKS WITH FORM-DATA
-# @app.route('/api/v1/resources/text/', methods=['POST'])
-# def summarize_from_text():
+## THIS ONE WORKS WITH FORM-DATA
+@app.route('/api/v1/resources/text/', methods=['POST'])
+def summarize_from_text():
     
-#     data = request.form["data"]
-#     summary = bert_sum(data)
+    data = request.form["data"]
+    summary = bert_sum(data)
 
-#     return summary
+    return summary
 
-# ## THIS ONE WORKS WITH DOCX FILES
-# @app.route('/api/v1/resources/document/summary', methods=['GET', 'POST'])
-# def summarize_from_file():
+## THIS ONE WORKS WITH DOCX FILES
+@app.route('/api/v1/resources/document/summary', methods=['GET', 'POST'])
+def summarize_from_file():
     
-#     f = request.files['data']
-#     f.save('datafile.docx')
-#     document = Document('datafile.docx')
-#     text =''
-#     for para in document.paragraphs:
-#         text+=para.text
+    f = request.files['data']
+    f.save('datafile.docx')
+    document = Document('datafile.docx')
+    text =''
+    for para in document.paragraphs:
+        text+=para.text
     
-#     summary = bert_sum(text)
+    summary = bert_sum(text)
 
-#     return summary
+    return summary
 
-# ## THIS ONE WORKS WITH DOCX FILES
-# @app.route('/api/v1/resources/document/docx', methods=['GET', 'POST'])
-# def return_document():
-#     f = request.files['data']
-#     f.save('datafile.docx')
+## THIS ONE WORKS WITH DOCX FILES
+@app.route('/api/v1/resources/document/docx', methods=['GET', 'POST'])
+def return_document():
+    f = request.files['data']
+    f.save('datafile.docx')
 
-#     return send_file('datafile.docx', attachment_filename='test.docx')
+    return send_file('datafile.docx', attachment_filename='test.docx')
 
-# def transform():
-#     data = 'datafile.docx'
-#     document = Document(data)
-#     new = Document()
-#     text =''
-#     for para in document.paragraphs:
-#         text+=para.text
-#     new.add_paragraph(bert_sum(text))
-#     return new
+def transform():
+    data = 'datafile.docx'
+    document = Document(data)
+    new = Document()
+    text =''
+    for para in document.paragraphs:
+        text+=para.text
+    new.add_paragraph(bert_sum(text))
+    return new
 
-# ## Allows for HTML interface
-# @app.route('/transform', methods=["GET","POST"])
-# def transform_view():
-#     f = request.files['data_file']
-#     f.save('datafile.docx')
-#     if not f:
-#         return "Please upload a word document"
-#     result = transform()
-#     result.save('result.docx')
-#     return send_file('result.docx', attachment_filename='new_file.docx')
+## Allows for HTML interface
+@app.route('/transform', methods=["GET","POST"])
+def transform_view():
+    f = request.files['data_file']
+    f.save('datafile.docx')
+    if not f:
+        return "Please upload a word document"
+    result = transform()
+    result.save('result.docx')
+    return send_file('result.docx', attachment_filename='new_file.docx')
 
 ### THIS ONE WORKS WITH FORM-DATA
 @app.route('/api/v1/resources/document/links', methods=['POST'])
